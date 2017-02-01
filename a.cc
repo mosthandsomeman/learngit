77c5139d2b5b3963abd02d02cef8a14e6ff600ba
mosthandsomeman <1539954482@qq.com>
2017-02-01 17:09:20 +0800

    add 2 files

---
+++ a.cc
@@ -0,0 +1,26 @@
+ ///
+ /// @file    a.cc
+ /// @author  FZC(@1539954482@qq.com)
+ /// @date    2017-02-01 16:45:20
+ ///
+ 
+#include <iostream>
+#include<string>
+using std::cout;
+using std::endl;
+using std::string;
+int main()
+{
+	#if 0
+	int i=0;
+	int sum=0;
+	while(i<20)
+		sum+=i++;
+	cout<<"sum="<<sum<<endl;
+	return 0;
+	#endif
+	string s1="hello";
+	cout<<s1<<endl;
+	return 0;
+}
+
